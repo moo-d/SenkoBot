e37b2ebd25b4e7235d02f1a161f180f808d3a5fd
ナスワサルサ <75057738+moo-d@users.noreply.github.com>
2021-09-29 23:30:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 let { create, Client } = require('@open-wa/wa-automate');
 let options = require('./options');
+let msgHandler = require('./main');
+
+function startSenko(Senko = new Client) {
+  console.log('[SERVER] Server Started!')
+  Senko.onMessage((async (message) => {
+    Senko.getAmountOfLoadedMessages()
+    .then((msg) => {
+      if (msg >= 3000) {
+        Senko.cutMsgCache()
+      }
+    })
+    msgHandler(Senko, message)
+  }))
+}
